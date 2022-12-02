b31ee488e13490f480014da1dddfcdeb7972ed18
Alexander Brewer <abrewer@forallsecure.com>
2022-12-02 17:32:52 -0500

    Stop script

--- mapi/runmapi.sh
+++ mapi/runmapi.sh
@@ -4,7 +4,7 @@ TARGET_URI="http://localhost:8080/"
 
 # Run mAPI
 mapi run forallsecure/grpc-web-echo-example \
-60 \
+10 \
 --url $TARGET_URI \
 "../net/grpc/gateway/examples/echo/echo.swagger.json" \
 --har out.har \
---
+++ mapi/stop-all.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker kill commonjs-client envoy-proxy node-server
